0450d38797724554294c302ad5094cdb526386c0
maximepakula <mpakula@takima.fr>
2024-12-06 00:12:08 +0100

    [1] tech: Init the db and app
    * [1] Added dockerfile and env file
    
    * [1] Added Spring app
    
    * [1] Fix

---
+++ src/main/java/maxime/pakula/daily_highlights_api/DailyHighlightsApiApplication.java
@@ -0,0 +1,13 @@
+package maxime.pakula.daily_highlights_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHighlightsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHighlightsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maxime/pakula/daily_highlights_api/DailyHighlightsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package maxime.pakula.daily_highlights_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyHighlightsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
